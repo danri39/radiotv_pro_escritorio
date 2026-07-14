7461a2b9d92b7c34293b0ef1b1f43a1c12866b07
DRS <danri39@gmail.com>
2026-07-14 13:41:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/drs/radiotv_pro_escritorio/RadiotvProEscritorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.drs.radiotv_pro_escritorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiotvProEscritorioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiotvProEscritorioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/drs/radiotv_pro_escritorio/RadiotvProEscritorioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drs.radiotv_pro_escritorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiotvProEscritorioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

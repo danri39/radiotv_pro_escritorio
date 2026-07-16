fa20a76ef2ad95544989c0a7101667121751e168
DRS <danri39@gmail.com>
2026-07-16 14:57:36 -0300

    Acertos

--- src/main/java/br/com/drs/radiotv_pro_escritorio/controller/ContratoPagamentoController.java
+++ src/main/java/br/com/drs/radiotv_pro_escritorio/controller/ContratoPagamentoController.java
@@ -1,4 +1,47 @@
 package br.com.drs.radiotv_pro_escritorio.controller;
 
+import br.com.drs.radiotv_pro_escritorio.dto.ContratoPagamentoDTO;
+import br.com.drs.radiotv_pro_escritorio.model.ContratoPagamento;
+import br.com.drs.radiotv_pro_escritorio.service.ContratoPagamentoService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/contratoPagamento")
+@RequiredArgsConstructor
 public class ContratoPagamentoController {
+
+    private final ContratoPagamentoService service;
+
+    @PostMapping
+    public ResponseEntity<String> salvar(@RequestBody ContratoPagamentoDTO dto) {
+        service.salvar(dto);
+        return ResponseEntity.ok("Pagamento salvo com sucesso!");
+    }
+
+    @GetMapping
+    public List<ContratoPagamento> listarTodos() {
+        return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ContratoPagamento> buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizar(@PathVariable Long id, @RequestBody ContratoPagamentoDTO dto) {
+        service.atualizar(id, dto);
+        return ResponseEntity.ok("Contrato bancario atualizado com sucesso!");
+    }
+
+    @DeleteMapping("/{id}")
+    public void apagar(@PathVariable Long id) {
+        service.apagar(id);
+    }
 }
--- src/main/java/br/com/drs/radiotv_pro_escritorio/controller/RamoAtividadeController.java
+++ src/main/java/br/com/drs/radiotv_pro_escritorio/controller/RamoAtividadeController.java
@@ -1,4 +1,46 @@
 package br.com.drs.radiotv_pro_escritorio.controller;
 
+import br.com.drs.radiotv_pro_escritorio.dto.RamoAtividadeDTO;
+import br.com.drs.radiotv_pro_escritorio.model.RamoAtividade;
+import br.com.drs.radiotv_pro_escritorio.service.RamoAtividadeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/ramoAtividade")
+@RequiredArgsConstructor
 public class RamoAtividadeController {
+
+    private final RamoAtividadeService service;
+
+    @PostMapping
+    public ResponseEntity<String> salvar(@RequestBody RamoAtividadeDTO dto) {
+        service.salvar(dto);
+        return ResponseEntity.ok("Ramo atividade salvo com sucesso!");
+    }
+
+    @GetMapping
+    public List<RamoAtividade> listarTodos() {
+        return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<RamoAtividade> buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> atualizar(@PathVariable Long id, @RequestBody RamoAtividadeDTO dto) {
+        service.atualizar(id, dto);
+        return ResponseEntity.ok("Ramo de atividade atualizado com sucesso!");
+    }
+
+    @DeleteMapping("/{id}")
+    public void apagar(@PathVariable Long id) {
+        service.apagar(id);
+    }
 }

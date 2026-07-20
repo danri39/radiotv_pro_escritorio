7f2daa1680dc6103d4cfdda35b0ffe6239220288
DRS <danri39@gmail.com>
2026-07-20 18:59:41 -0300

    Atualizações

--- src/main/java/br/com/drs/radiotv_pro_escritorio/repository/FolhaPagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.drs.radiotv_pro_escritorio.repository;
-
-import br.com.drs.radiotv_pro_escritorio.model.FolhaPagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FolhaPagamentoRepository extends JpaRepository<FolhaPagamento, Long> {
-}
